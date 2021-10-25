41b59c97abdb06b52632659b825ced56ff6aef50
Carlos Rojas <carlosrb206@gmail.com>
2021-10-24 20:09:02 -0400

    delete wrong readme file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,33 +7,31 @@ const renderLicenseSection = licenseFuctions.licenseSection
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
-    # ${data.projectTitle} ${renderLicenseBadge(data.license)}${renderLicenseLink(data.license)}
+  # ${data.projectTitle} ${renderLicenseBadge(data.license)}${renderLicenseLink(data.license)}
+  ## Table of Contents
+  1. [Description](#Description)
+  2. [Installation](#Installation)
+  3. [Usage](#Usage)
+  4. [Contributing](#Contributing)
+  5. [Tests](#Tests)
+  6. [License](#License)
+  7. [Questions](#Questions)
+  ## Description
+  ${data.projectDescription}
+  ## Installation
+  ${data.projectInstallation}
+  ## Usage
+  ${data.projectUsage}
+  ## Contributing
+  ${data.projectContribution}
+  ## Tests
+  ${data.projectTest}
+  ## License
+  ${renderLicenseSection(data.license)}
     
-    ## Table of Contents
-    1. [Description](#Description)
-    2. [Installation](#Installation)
-    3. [Usage](#Usage)
-    4. [Contributing](#Contributing)
-    5. [Tests](#Tests)
-    6. [License](#License)
-    7. [Questions](#Questions)
-    
-    ## Description
-    ${data.projectDescription}
-    ## Installation
-    ${data.projectInstallation}
-    ## Usage
-    ${data.projectUsage}
-    ## Contributing
-    ${data.projectContribution}
-    ## Tests
-    ${data.projectTest}
-    ## License
-    ${renderLicenseSection(data.license)}
-    
-    ## Questions
-    https://github.com/${data.userGitHub}\n
-    Please email ${data.userEmail} for questions.
+  ## Questions
+  https://github.com/${data.userGitHub}\n
+  Please email ${data.userEmail} for questions.
   `;
 }
 
